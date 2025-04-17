d9c012342d7981f9d1270d991c8878916516ef49
enwoge <eberechinwoge@gmail.com>
2025-04-17 10:11:27 +0100

    practice more use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Counter from "./Counter"
 import MyComponent from "./MyComponent"
 import Practice from "./Practice"
+import TodoList from "./TodoList"
 
 function App() {
   return (
     <div>
       {/* <MyComponent /> */}
-      <Practice />
+      {/* <Practice /> */}
+      <TodoList />
     </div>
   )
 }
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -41,6 +41,37 @@ const MyComponent = () => {
         setFoods(foods.filter((_, i) => i !== index))
     }
 
+    const [cars, setCars] = useState(['2011 Camrey bash']);
+    const [carYear, setCarYear] = useState(new Date().getFullYear());
+    const [carMake, setCarMake] = useState('');
+    const [carModel, setCarModel] = useState('');
+
+    function AddCar() {
+        const newCar = {
+            year: carYear,
+            make: carMake,
+            model: carModel,
+        }
+
+        setCars(c => [...c, newCar]);
+        setCarYear(new Date().getFullYear());
+        setCarMake("");
+        setCarModel("");
+    }
+    function RemoveCar(index) {
+        setCars(c => c.filter((_, i) => i !== index));
+    }
+    function YearChange(e) {
+        setCarYear(e.target.value);
+    }
+    function MakeChange(e) {
+        setCarMake(e.target.value);
+    }
+    function ModelChange(e) {
+        setCarModel(e.target.value)
+    }
+
+
     return (
         <div className='main-container'>
             <div>
@@ -87,6 +118,23 @@ const MyComponent = () => {
                 </ul>
                 <input type="text" className='food-input' placeholder='Enter Food Name'/>
                 <button onClick={handleAddFood}>Add Food</button>
+            </div>
+
+            <div>
+                <h2>List of Car Objects</h2>
+                <ul>
+                    {cars.map((car, index) => (
+                        car.year && car.make && car.model && (
+                            <li key={index} onClick={() => RemoveCar(index)}>
+                                {car.year} {car.make} {car.model}
+                            </li>))
+                            )
+                    }
+                </ul>
+                <input type="number" value={carYear} onChange={YearChange}/>
+                <input type="text" value={carMake} onChange={MakeChange} placeholder='Enter Car Make'/>
+                <input type="text" value={carModel} onChange={ModelChange} placeholder='Enter Car Model'/>
+                <button onClick={AddCar}>Add Car</button> 
             </div>
         </div>
     )
